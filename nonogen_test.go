46520af1dad47e730ad2d948b89637b082ee940f
pacaudj <jean-baptiste.pacaud-paris@epitech.eu>
2021-05-01 13:07:26 +0200

    updated tests so they don't create files anymore

--- nonogen_test.go
+++ nonogen_test.go
@@ -38,23 +38,22 @@ func TestNonogen(b *testing.T) {
 		name   string
 		dest   string
 	}{
-		{10, 30000, "test-pic/bird.jpg", "test-output/bird_10.jpg"},
-		{15, 30000, "test-pic/bird.jpg", "test-output/bird_15.jpg"},
-		{20, 30000, "test-pic/bird.jpg", "test-output/bird_20.jpg"},
-		{30, 25000, "test-pic/shosta.jpg", "test-output/shosta_1.jpg"},
-		{30, 30000, "test-pic/shosta.jpg", "test-output/shosta_2.jpg"},
-		{30, 35000, "test-pic/shosta.jpg", "test-output/shosta_3.jpg"},
-		{20, 30000, "test-pic/lion.jpg", "test-output/lion_1.jpg"},
-		{30, 30000, "test-pic/lion.jpg", "test-output/lion_2.jpg"},
-		{55, 30000, "test-pic/lion.jpg", "test-output/lion_3.jpg"},
+		{10, 30000, "test-pic/bird.jpg", "bird_10.jpg"},
+		{15, 30000, "test-pic/bird.jpg", "bird_15.jpg"},
+		{20, 30000, "test-pic/bird.jpg", "bird_20.jpg"},
+		{30, 25000, "test-pic/shosta.jpg", "shosta_1.jpg"},
+		{30, 30000, "test-pic/shosta.jpg", "shosta_2.jpg"},
+		{30, 35000, "test-pic/shosta.jpg", "shosta_3.jpg"},
+		{20, 30000, "test-pic/lion.jpg", "lion_1.jpg"},
+		{30, 30000, "test-pic/lion.jpg", "lion_2.jpg"},
+		{55, 30000, "test-pic/lion.jpg", "lion_3.jpg"},
 	}
 	for _, table := range tables {
-		nono, err := nonogen.NonoGen(table.size, table.bright, table.name)
+		_, err := nonogen.NonoGen(table.size, table.bright, table.name)
 		if err != nil {
 			os.Exit(1)
 		} else {
-			nonogen.NonoToJPG(nono, table.dest)
-			fmt.Println("Successfully generated file", table.dest)
+			fmt.Println("Successfully generated nonogram", table.dest)
 		}
 	}
 }
